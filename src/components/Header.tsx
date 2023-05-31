03d1a56b50aaa6b3c1bea1d675594057e919783f
art <arthurvat7@gmail.com>
2023-05-31 13:07:05 -0700

    switch font to Billy-Light

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,8 @@ import styled from "styled-components";
 type Props = {};
 
 const Icon = styled(SocialIcon)`
-  height: 3.5em !important;
-  width: 3.5em !important;
+  height: 2em !important;
+  width: 2em !important;
 
   @media (max-width: 623px) {
     height: 3em !important;

ca67017ce815827a661f8240e9c6a58802c83c2a
art <arthurvat7@gmail.com>
2023-05-31 00:30:55 -0700

    social links now open in a new tab

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,42 +23,49 @@ const Header = (props: Props) => {
         url="https://github.com/avannak"
         fgColor="white"
         bgColor="transparent"
+        target="_blank"
       ></Icon>
       <Icon
         className="social"
         url="https://www.linkedin.com/in/arthur-vannakittikun-b7024a190/"
         fgColor="white"
         bgColor="transparent"
+        target="_blank"
       ></Icon>
       <Icon
         className="social"
         url="https://www.youtube.com/arthurvmusic"
         fgColor="white"
         bgColor="transparent"
+        target="_blank"
       ></Icon>
       <Icon
         className="social"
         url="https://soundcloud.com/arthurvofficial"
         fgColor="white"
         bgColor="transparent"
+        target="_blank"
       ></Icon>
       <Icon
         className="social"
         url="https://open.spotify.com/artist/4tUwnPevkVbKAvX8welQYT?si=T65-uDYdQBGFKZdoA260JQ"
         fgColor="white"
         bgColor="transparent"
+        target="_blank"
       ></Icon>
       <Icon
         className="social"
         url="https://www.instagram.com/arthurvkk/"
         fgColor="white"
         bgColor="transparent"
+        target="_blank"
       ></Icon>
       <Icon
         className="social"
         url="https://mail.google.com/mail/u/0/?fs=1&tf=cm&to=arthurvat7@gmail.com&body"
         fgColor="white"
         bgColor="transparent"
+        target="_blank"
       ></Icon>
     </div>
   );

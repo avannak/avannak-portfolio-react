00577e00cd09c7356163e8aff0b173524c008bd4
art <arthurvat7@gmail.com>
2023-06-03 14:08:17 -0700

    final deployment before resume is added

--- src/PageComponents/contact/ContactForm.tsx
+++ src/PageComponents/contact/ContactForm.tsx
@@ -66,7 +66,7 @@ const ContactForm = (props: Props) => {
               pattern: {
                 value:
                   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                message: "Please enter a valid email",
+                message: "Please enter a valid email.",
               },
             })}
             id="email"
